e05760e63b8bf338ed5ddb282e378c2744ebdd15
Riley Flynn <riley@rileyflynn.me>
2017-04-01 22:30:14 -0230

    Made the base ChainmailPlugin class have a logger created on the fly as it is required

--- Chainmail/Plugin.py
+++ Chainmail/Plugin.py
@@ -1,3 +1,4 @@
+import logging
 from jigsaw import JigsawPlugin
 
 from . import Wrapper
@@ -7,3 +8,10 @@ class ChainmailPlugin(JigsawPlugin):
     def __init__(self, manifest: dict, wrapper: "Wrapper.Wrapper") -> None:
         super().__init__(manifest)
         self.wrapper = wrapper
+        self._logger = None
+
+    @property
+    def logger(self) -> logging.Logger:
+        if self._logger is None:
+            self._logger = logging.getLogger(self.manifest.get("name"))
+        return self._logger
