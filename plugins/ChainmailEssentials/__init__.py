6d3bb21d25ae1e8d496c1ff726535813a43f0688
Riley Flynn <riley@rileyflynn.me>
2017-04-08 15:39:38 -0230

    Added command that gives list of plugins

--- plugins/ChainmailEssentials/__init__.py
+++ plugins/ChainmailEssentials/__init__.py
@@ -7,7 +7,8 @@ class ChainmailEssentials(ChainmailPlugin):
     def __init__(self, manifest: dict, wrapper: "Wrapper.Wrapper") -> None:
         super().__init__(manifest, wrapper)
 
-        self.commands_no_args = self.wrapper.CommandRegistry.register_command("!commands", "^!commands$", "Lists commands accessible to a user.", self.command_commands)
+        self.commands = self.wrapper.CommandRegistry.register_command("!commands", "^!commands$", "Lists commands accessible to a user.", self.command_commands)
+        self.plugins = self.wrapper.CommandRegistry.register_command("!plugins", "^!plugins$", "Lists all loaded plugins.", self.command_plugins)
 
     def command_commands(self, event: CommandSentEvent):
         commands = self.wrapper.CommandRegistry.get_accessible_commands(event.player)
@@ -19,4 +20,19 @@ class ChainmailEssentials(ChainmailPlugin):
                 builder.add_field(f"{command.name}: ", Colours.red)
                 suffix = "\n" if command != commands[-1] and command.name != commands[-1].name else ""
                 builder.add_field(f"{command.description}{suffix}", Colours.gold)
+        event.player.send_message(builder)
+
+    def command_plugins(self, event: CommandSentEvent):
+        plugins = self.wrapper.plugin_manager.get_all_plugins()
+        builder = MessageBuilder(self.wrapper)
+
+        for plugin in plugins:
+            if self.wrapper.plugin_manager.get_plugin_loaded(plugin["manifest"]["name"]):
+                builder.add_field(f"{plugin['manifest']['name']}\n", Colours.blue)
+                builder.add_field("    Developer: ", Colours.red)
+                builder.add_field(f"{plugin['manifest']['developer']}\n", Colours.blue)
+                suffix = "\n" if plugin != plugins[-1] else ""
+                builder.add_field("    Version: ", Colours.red)
+                builder.add_field(f"{plugin['manifest']['version']}{suffix}", Colours.blue)
+
         event.player.send_message(builder)
