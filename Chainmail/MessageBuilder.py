601c520d676df7623d80a7fe77ab8f2a667f7235
Riley Flynn <riley@rileyflynn.me>
2017-04-09 19:02:20 -0230

    Added method documentation for MessageBuilder related methods

--- Chainmail/MessageBuilder.py
+++ Chainmail/MessageBuilder.py
@@ -28,15 +28,35 @@ class HoverEvent(object):
 
     # noinspection PyMethodMayBeStatic
     def to_dict(self) -> dict:
+        """
+        Returns a dict for this HoverEvent
+        :return: The dict
+        """
         return {}
 
 
 class MessageBuilder(object):
 
     def __init__(self):
+        """
+        Initializes a new message builder
+        """
         self.fields = []
 
     def add_field(self, text: str, colour: Colours=Colours.white, bold: bool=False, italic: bool=False, underlined: bool=False, strikethrough: bool=False, obfuscated: bool=False, hover_event: HoverEvent=HoverEvent(), insertion: str="",  **kwargs):
+        """
+        Adds a new field to the message builder
+        :param text: The text of the field
+        :param colour: The colour of the field
+        :param bold: Whether or not this field is bold
+        :param italic: Whether or not this field is italic
+        :param underlined: Whether or not this field is underlined
+        :param strikethrough: Whether or not this field has a strikethrough
+        :param obfuscated: Whether or not this field is obfuscated
+        :param hover_event: The hover event for this field
+        :param insertion: What will be inserted when you shift-click this field
+        :param kwargs: Any additional arguments
+        """
         self.fields.append({
             "text": text,
             "color": colour.value,
@@ -51,12 +71,25 @@ class MessageBuilder(object):
         })
 
     def send(self, destination: "Player.Player"):
+        """
+        Sends this message to a player
+        :param destination: The player to sent this message to
+        """
         destination.send_message(self)
 
     def generate_command(self, player: "Player.Player") -> str:
+        """
+        Generates the command to be used to send this message
+        :param player: The player to send the message to
+        :return: The command
+        """
         return f"tellraw {player.username} {json.dumps(self.fields)}"
 
     def send_to_group(self, group: List["Player.Player"]):
+        """
+        Sends this message to a group of people
+        :param group: The people to send this to
+        """
         for player in group:
             player.send_message(self)
 
@@ -64,9 +97,22 @@ class MessageBuilder(object):
 class TextHoverEvent(HoverEvent):
 
     def __init__(self):
+        """
+        Initializes a new TextHoverEvent
+        """
         self.builder = MessageBuilder()
 
     def add_field(self, text: str, colour: Colours=Colours.white, bold: bool=False, italic: bool=False, underlined: bool=False, strikethrough: bool=False, obfuscated: bool=False):
+        """
+        Adds a new field to the message builder
+        :param text: The text of the field
+        :param colour: The colour of the field
+        :param bold: Whether or not this field is bold
+        :param italic: Whether or not this field is italic
+        :param underlined: Whether or not this field is underlined
+        :param strikethrough: Whether or not this field has a strikethrough
+        :param obfuscated: Whether or not this field is obfuscated
+        """
         self.builder.add_field(text=text,
                                colour=colour,
                                bold=bold,
@@ -76,6 +122,10 @@ class TextHoverEvent(HoverEvent):
                                obfuscated=obfuscated)
 
     def to_dict(self) -> dict:
+        """
+        Returns a dict for this HoverEvent
+        :return: The dict
+        """
         return {
             "action": "show_text",
             "value": {"text": "",
