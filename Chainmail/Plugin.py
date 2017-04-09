2c4575be704788db275e3563657b7a2b51d9fdf0
Riley Flynn <riley@rileyflynn.me>
2017-04-09 19:11:57 -0230

    Added Plugin related method documentation

--- Chainmail/Plugin.py
+++ Chainmail/Plugin.py
@@ -6,12 +6,21 @@ from . import Wrapper
 
 class ChainmailPlugin(JigsawPlugin):
     def __init__(self, manifest: dict, wrapper: "Wrapper.Wrapper") -> None:
+        """
+        Initializes a new plugin
+        :param manifest: The plugin manifest
+        :param wrapper: The wrapper that loaded this plugin
+        """
         super().__init__(manifest)
         self.wrapper = wrapper
         self._logger = None
 
     @property
     def logger(self) -> logging.Logger:
+        """
+        Gets the logger belonging to this plugin
+        :return: The logger
+        """
         if self._logger is None:
             self._logger = logging.getLogger(self.manifest.get("name"))
         return self._logger
