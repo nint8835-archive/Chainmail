f789f0fc94b33af18fd4c820b74fa830ad9b1ae0
Riley Flynn <riley@rileyflynn.me>
2017-04-12 19:16:36 -0230

    Added new_version_available property to the plugin base class to allow for easy checking for updates

--- Chainmail/Plugin.py
+++ Chainmail/Plugin.py
@@ -1,4 +1,7 @@
 import logging
+from typing import Optional
+
+import requests
 from jigsaw import JigsawPlugin
 
 from . import Wrapper
@@ -31,3 +34,25 @@ class ChainmailPlugin(JigsawPlugin):
 
     def disable(self) -> None:
         self.enabled = False
+
+    @property
+    def new_version_available(self) -> Optional[bool]:
+        """
+        Checks the remote manifest to see if a new version is available
+        :return: Whether or not there is a new version available
+        """
+        if self.manifest.get("remote_manifest", "") == "":
+            return
+        self.logger.info("Checking for update...")
+        try:
+            manifest = requests.get(self.manifest.get("remote_manifest", "")).json()
+            version_remote = manifest["version"].split(".")
+            version_local = self.manifest["version"].split(".")
+            for i in range(len(version_local)):
+                if int(version_local[i]) < int(self.get_item_from_list(version_remote, i, "0")):
+                    self.logger.info(f"An update is available. Current version is v{self.manifest['version']}, updated version is v{manifest['version']}.")
+                    return True
+            self.logger.info("No update required.")
+            return False
+        except requests.HTTPError:
+            self.logger.warning("Failed to check for update.")
