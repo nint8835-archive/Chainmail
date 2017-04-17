e1f0984741ee89b7fbe467217dc851e32eb3c1f2
Riley Flynn <riley@rileyflynn.me>
2017-04-17 18:40:49 -0230

    Removed unneccesary plugins Test Plugin and Auto OP

--- plugins/TestPlugin/__init__.py
+++
@@ -1,7 +0,0 @@
-from Chainmail import Wrapper
-from Chainmail.Plugin import ChainmailPlugin
-
-
-class TestPlugin(ChainmailPlugin):
-    def __init__(self, manifest: dict, wrapper: "Wrapper.Wrapper") -> None:
-        super().__init__(manifest, wrapper)
